770b8933ba96f96c53aaf454d6d95dba0cc8f02c
unknown <nadjam@326-F2.nad.qc.ca>
2017-08-18 22:44:18 -0400

    Inventory first commit

---
+++ Flamingos/Assets/Scripts/Inventory/InventoryManager.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InventoryManager : Singleton<InventoryManager>
+{
+    private int total_money;
+    private int total_bottles;
+    private int total_empty_bottles;
+
+    public void BeginNewGame()
+    {
+        total_money = 100;
+        total_bottles = 10;
+    }
+
+    public void AddMoney(int money)
+    {
+        total_money += money;
+    }
+
+    public void RemoveMoney(int cost)
+    {
+        total_money -= cost;
+    }
+
+    public void AddBottles(int amount_bottles)
+    {
+        total_bottles += amount_bottles;
+    }
+
+    public void RemoveBottles(int lost_bottles)
+    {
+        total_bottles += lost_bottles;
+    }
+}
