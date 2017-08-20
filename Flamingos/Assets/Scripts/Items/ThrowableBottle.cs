a4201631bdbe525a1777d92a56f6fabc91a783d2
Kévin Duguay <kevin.duguay1@gmail.com>
2017-08-20 06:01:31 -0400

    Money is gained when the bottles hit a client

--- Flamingos/Assets/Scripts/Items/ThrowableBottle.cs
+++ Flamingos/Assets/Scripts/Items/ThrowableBottle.cs
@@ -4,6 +4,17 @@ public class ThrowableBottle : MonoBehaviour
 {
     private  void OnCollisionEnter(Collision collision)
     {
-        if (collision.transform.tag == GameManager.ROOM_TAG) Destroy(gameObject);
+        switch (collision.transform.tag)
+        {
+            case GameManager.ROOM_TAG:
+                Destroy(gameObject);
+                break;
+            case GameManager.DRUNK_TAG:
+                Destroy(gameObject);
+                break;
+            case GameManager.CLIENT_TAG:
+                InventoryManager.GetInstance().AddMoney(GetComponent<Item>().Gain);
+                break;
+        }
     }
 }
